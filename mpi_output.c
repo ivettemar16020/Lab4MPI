86c787cfee05f4b000f0bbb29bbda149663e2d9c
Ivette Cardona <ivettecardona@ivettes-mbp.lan>
2020-03-15 20:08:10 -0600

    3 - orden descendente

--- mpi_output.c
+++ mpi_output.c
@@ -15,32 +15,17 @@
 #include <mpi.h> 
 
 int main(int argc, char **argv) {
-   int my_rank, comm_sz, flag, i;
-   char message[] = "Alguien tiene un palillo?";
+  int my_rank, comm_sz, i;
 
-   MPI_Init(&argc, &argv); 
-   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
-   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
-
-   if (my_rank == 0) {
-      MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-      printf("1 SIZE = %d RANK = %d MESSAGE = %s \n",comm_sz,my_rank, message);
-    } else {
-        int buffer;
-        MPI_Status status;
-        MPI_Probe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-        MPI_Get_count(&status, MPI_INT, &buffer);
-      if (buffer == 1) {
-        printf("2 SIZE = %d RANK = %d MESSAGE = %s \n",comm_sz,my_rank, message);
-        MPI_Recv(&message, buffer, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-          if (my_rank + 1 != comm_sz) {
-          MPI_Send(&message, 1, MPI_INT, ++my_rank, 0, MPI_COMM_WORLD);
-          }
-        };
-      };
-
-      //printf("Proc %d of %d Alguien tiene un palillo?\n", my_rank, comm_sz);
-   
+  MPI_Init(NULL, NULL); 
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+  for(i = comm_sz - 1; i>= 0; i--){
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (i == my_rank){
+      printf("Proc %d of %d > Alguien tiene un palillo?\n", my_rank, comm_sz);
+    }
+  }
 
    MPI_Finalize();
    return 0;
